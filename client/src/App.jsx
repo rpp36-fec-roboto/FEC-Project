fbd94f094e0827b0f0c7345b46384e0c1db6380e
Ziqian Li <liziqian1992@gmail.com>
2022-06-27 18:00:26 -0700

    add structure in overview widget

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Overview from './Widgets/Overview/Overview.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      productId: 0
+    }
   }
 
   render() {
-    return <h1>Hello</h1>
+    return (
+      <Overview productId={this.state.productId} />
+    )
   }
 }
 

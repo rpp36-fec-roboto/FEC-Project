3fff3f275e015523c2b7fbba772ad42a93b27f71
Ziqian Li <liziqian1992@gmail.com>
2022-06-29 20:59:15 -0700

    added productInfo sample data from API

--- client/src/data/sampleData.js
+++ client/src/data/sampleData.js
@@ -56,6 +56,28 @@ module.exports.products = [
   }
 ];
 
+module.exports.productInfo = {
+    "id": 71697,
+    "campus": "hr-rpp",
+    "name": "Camo Onesie",
+    "slogan": "Blend in to your crowd",
+    "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
+    "category": "Jackets",
+    "default_price": "140.00",
+    "created_at": "2022-05-11T19:38:15.373Z",
+    "updated_at": "2022-05-11T19:38:15.373Z",
+    "features": [
+        {
+            "feature": "Fabric",
+            "value": "Canvas"
+        },
+        {
+            "feature": "Buttons",
+            "value": "Brass"
+        }
+    ]
+}
+
 module.exports.productStyle = {
   "product_id": "71697",
   "results": [

adf952d15356cc3b3e3ef818edd54b52a20ab321
Ziqian Li <liziqian1992@gmail.com>
2022-06-29 22:17:42 -0700

    added data and rendered some information from the data

--- client/src/components/Overview/ProductInfo.jsx
+++ client/src/components/Overview/ProductInfo.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import Style from './Style.jsx';
+
 var ProductInfo = (props) => {
+  var rating = props.rating;
+  var productInfo = props.productInfo; // initiate by GET GET /products/:product_id
+
+  return (
+    <div className="col-1-3">
+      <div className="rating">
+        <p>rating: {rating}/5</p>
+        <a>Read all reviews</a> {/* links to reviews widget */}
+      </div>
+
+      <div>
+        <p>{productInfo.category}</p>
+        <h3>{productInfo.name}</h3>
+      </div>
+    </div>
+  );
+
   // star rating
   // product category
   // product title
   // price: style specific, may be on sale
   // product overview: may be null
+  // add to Cart
   // star to click to add to MyOutfit
 };
 

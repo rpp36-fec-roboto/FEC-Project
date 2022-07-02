11da6135fb43b3fe906be00b0a3e21383511ce4a
Ziqian Li <liziqian1992@gmail.com>
2022-07-01 21:03:24 -0700

    build skeleton of components

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -5,6 +5,24 @@ var ImageGallery = (props) => {
   // default view
   // expaned view
 
+  return (
+    <div>
+      <div>Default view
+        <div>current selected image. Default is first image</div>
+        <div>thumbnail images of overlaying the default view image. Selection match current view.
+          up to 7 thumbnail images in a column
+        <button>arrow to scroll for more thumbnail images</button>
+        </div>
+        <button>Right arrow</button>
+        <button>Left arrow</button>
+      </div>
+
+      <div>Expaneded view
+        <div>current image overlaying entire overview besides other info</div>
+      </div>
+    </div>
+  );
+
 };
 
 export default ImageGallery;

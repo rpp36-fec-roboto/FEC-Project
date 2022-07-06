855a51b40c0ea71477ed706634442a1f80d574d8
Michael Manzo <mpmanzo@gmail.com>
2022-07-06 16:27:06 -0600

    Modified components and added CSS to display in a carousel fashion

--- client/src/components/relatedItems/YourOutfitList.jsx
+++ client/src/components/relatedItems/YourOutfitList.jsx
@@ -4,27 +4,71 @@ import RelatedProductCard from './RelatedProductCard.jsx';
 import sampleData from '../../data/sampleData.js';
 
 var YourOutfitList = function (props) {
-  let [yourOutfit, setYourOutfit] = useState([71697]); // update
+  const productId = props.productId;
+  const productInfo = sampleData.productInfo;
+  const productStyle = sampleData.productStyle;
+  const [yourOutfit, setYourOutfit] = useState(props.yourOutfit);
+  const [show, setShow] = useState(4);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [length, setLength] = useState(yourOutfit.length);
 
-  let items = yourOutfit.map((item) => {
+  let products = yourOutfit.map((id) => {
     return (
-      <ul key={item}>
-        <li style={{ 'listStyleType': 'none' }}>
+      <div key={id.toString()}>
+        <div style={{ padding: 8 }}>
           <RelatedProductCard
             list={'yourOutfit'}
-            productId={item} // send 1st product only to start
+            productId={id}
           />
-        </li>
-      </ul>
+        </div>
+      </div>
     );
   });
 
-  return (
-    <div>
-      <h3>YOUR OUTFIT</h3>
-      {items}
-    </div>
+  // useEffect(() => {
+  //   setLength(products.length);
+  // }, [products]);
 
+  const next = () => {
+    if (currentIndex < (length - 1)) {
+      setCurrentIndex(prevState => prevState + 1);
+    }
+  };
+
+  const prev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(prevState => prevState - 1);
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: 1200, maxHeight: 100, marginLeft: 'auto', marginRight: 'auto', marginTop: 0 }}>
+      <h3>YOUR OUTFIT</h3>
+      <div className="ri-carousel-container">
+        <div className="ri-carousel-wrapper">
+          {
+            currentIndex > 0 &&
+            <button onClick={prev} className="ri-left-arrow">
+              &lt;
+            </button>
+          }
+          <div className="ri-carousel-content-wrapper">
+            <div
+              className={`ri-carousel-content show-${show}`}
+              style={{ transform: `translateX-${currentIndex * (100 / show)}%)` }}
+            >
+              {products}
+            </div>
+          </div>
+          {
+            currentIndex < (length - 1) &&
+            <button onClick={next} className="ri-right-arrow">
+              &gt;
+            </button>
+          }
+        </div>
+      </div>
+    </div>
   );
 };
 

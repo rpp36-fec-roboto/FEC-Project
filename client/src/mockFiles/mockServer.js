b742f61bd0dc5cfc99234e6ca46edf500e7e1e80
Ziqian Li <liziqian1992@gmail.com>
2022-07-22 12:10:17 -0700

    change file name and include a mockServer

---
+++ client/src/mockFiles/mockServer.js
@@ -0,0 +1,50 @@
+// import API mocking utilities from Mock Service Worker
+import {rest} from 'msw';
+import {setupServer} from 'msw/node';
+
+import sampleData from './sampleData.js';
+
+// declare which API requests to mock
+const server = setupServer(
+  rest.get('/products/:product_id', (res, req, ctx) => {
+    // response using a mocked JSON body
+    ctx.json(sampleData.productInfo);
+  }),
+
+  rest.get('/products/:product_id/styles', (req, res, ctx) => {
+    if (req.params['product_id'] === '71697') {
+      // valid dataset
+      return res(ctx.json(sampleData.productStyle));
+    } else {
+      // invalid dataset
+      return res(ctx.json(sampleData.invalidDataset));
+    }
+  }),
+
+  rest.get('/reviews/meta', (req, res, ctx) => {
+    return res(ctx.json(sampleData.reviewsMeta));
+  }),
+
+  rest.get('/products/:product_id/related', (req, res, ctx) => {
+    return res(ctx.json([71697]));
+  }),
+
+  rest.get('/qa/questions/:question_id/answers', (req, res, ctx) => {
+    return res(ctx.json(sampleData.answers));
+  }),
+
+  rest.get('/qa/questions', (req, res, ctx) => {
+    return res(ctx.json(sampleData.questions));
+  }),
+
+  rest.post('/interactions', (req, res, ctx) => {
+    const { element, widget, time } = req.body;
+    if (element && widget && time) {
+      return res(ctx.status(201));
+    } else {
+      return res(ctx.status(500));
+    }
+  })
+);
+
+export default server;

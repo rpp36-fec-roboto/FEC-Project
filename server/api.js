f975001706a0d10f4584fe7655ca2447850e6959
Ziqian Li <liziqian1992@gmail.com>
2022-07-25 10:53:44 -0700

    add dotenv and environment variable to project

--- server/api.js
+++ server/api.js
@@ -1,12 +1,11 @@
 const axios = require('axios');
-const API_KEY = require('./config.js');
+const API_KEY = process.env;
 
 const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/';
 const headers = { 'Authorization': `${API_KEY.TOKEN}` };
 
 let getData = (url, param, callback) => {
   let options = {
-    // url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${url}${param}`,
     url,
     baseURL,
     params: param,
